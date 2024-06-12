ef29dd072521184ff4a1aabdb412db2e9e9a7b52
Jack O'Connor <oconnor663@gmail.com>
2024-06-12 09:06:18 -0700

    use consistent Rust formatting

--- scream.rs/src/main.rs
+++ scream.rs/src/main.rs
@@ -1,7 +1,6 @@
-use std::io;
-use std::io::prelude::*;
+use std::io::{stdout, Result, Write};
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let path = std::env::args().nth(1);
     let mut output = ScreamingOutput::new(path)?;
     output.write("hello world\n")?;
@@ -14,7 +13,7 @@ enum ScreamingOutput {
 }
 
 impl ScreamingOutput {
-    fn new(maybe_path: Option<String>) -> io::Result<ScreamingOutput> {
+    fn new(maybe_path: Option<String>) -> Result<ScreamingOutput> {
         match maybe_path {
             Some(path) => {
                 let file = std::fs::File::create(path)?;
@@ -24,11 +23,15 @@ impl ScreamingOutput {
         }
     }
 
-    fn write(&mut self, string: &str) -> io::Result<()> {
+    fn write(&mut self, string: &str) -> Result<()> {
         let all_caps = string.to_uppercase();
         match self {
-            ScreamingOutput::File { file } => file.write_all(all_caps.as_bytes()),
-            ScreamingOutput::Stdout => std::io::stdout().write_all(all_caps.as_bytes()),
+            ScreamingOutput::File { file } => {
+                file.write_all(all_caps.as_bytes())
+            }
+            ScreamingOutput::Stdout => {
+                stdout().write_all(all_caps.as_bytes())
+            }
         }
     }
 }
